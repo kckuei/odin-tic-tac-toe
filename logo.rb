3634fa741932352d10c23c476afb27a5ef06e8ee
kkuei <kevinkuei@gmail.com>
2022-09-07 20:34:11 -0700

    Add function to check winner

--- logo.rb
+++ logo.rb
@@ -85,7 +85,7 @@ def format(val)
   return val.to_s.rjust(3, ' ')
 end
 
-def render_board(board)
+def draw_board(board)
   grid_size = board.length
   k = 0;
   for i in 0..grid_size-1 do
@@ -100,6 +100,7 @@ def render_board(board)
     end
     draw_row(grid_size) if i < grid_size-1
   end
+  puts "\n\n"
 end
 
 def valid_move(coords, board)
@@ -107,40 +108,69 @@ def valid_move(coords, board)
   return val.include?('x') || val.include('o')
 end
 
+def check_winner(avatar, board)
+  grid_size = board[0].length 
+  # check rows
+  for row in 0..grid_size-1 do
+    result = board[row].reduce{|acc, item| acc << item}
+	return true if result.include?(avatar * grid_size)
+  end
+  # check columns
+  for col in 0..grid_size-1 do
+    result = board.map{|row| row[col]}.join
+	return true if result.include?(avatar * grid_size)
+  end
+  # check diaganols
+  result = (0..grid_size-1).reduce(""){|acc, i| acc << board[i][i]}
+  return true if result.include?(avatar * grid_size)
+  result = (0..grid_size-1).reduce(""){|acc, i| acc << board[i][grid_size-1-i]}
+  return true if result.include?(avatar * grid_size)
+  
+  return false
+end
+
 # Test 3x3 board
 mapping = make_mapping(3)
 board = initialize_board(3)
 coords = mapping[5]
 board = update_board(coords, 'x', board)
-render_board(board)
-
-puts "\n\n"
+draw_board(board)
 
 # Test 5x5 board
 mapping = make_mapping(5)
 board = initialize_board(5)
 coords = mapping[5]
 board = update_board(coords, 'x', board)
-render_board(board)
-
-puts "\n\n"
+draw_board(board)
 
 # Test 7x7 board
 mapping = make_mapping(7)
 board = initialize_board(7)
 coords = mapping[7]
 board = update_board(coords, 'x', board)
-render_board(board)
-
-puts "\n\n"
+draw_board(board)
 
 # Test 9x9 board
 mapping = make_mapping(9)
 board = initialize_board(9)
-coords = mapping[9]
-board = update_board(coords, 'x', board)
-render_board(board)
-
+#(1..9).to_a.each do |i|
+#  coords = mapping[i]
+#  update_board(coords, 'x', board)
+#end
+#(1..73).step(9).to_a.each do |i|
+#  coords = mapping[i]
+#  update_board(coords, 'x', board)
+#end
+#(1..81).step(10).to_a.each do |i|
+#  coords = mapping[i]
+#  update_board(coords, 'x', board)
+#end
+(9..73).step(8).to_a.each do |i|
+  coords = mapping[i]
+  update_board(coords, 'x', board)
+end
+draw_board(board)
+puts check_winner('x', board)
 
 
 gets
@@ -158,8 +188,8 @@ gets
 cls
 puts grid
 grid_size = 0
-until [3, 5, 7].include?(grid_size.to_i)
-  puts "\nSelect your grid size (3, 5, 7)."
+until [3, 5, 7, 9].include?(grid_size.to_i)
+  puts "\nSelect your grid size (3, 5, 7, 9)."
   grid_size = gets.chomp
 end
 cls
@@ -171,8 +201,8 @@ puts "\n#{username1} (o) and #{username2} (x) will dual to the fate in a game of
 puts "\n...Champions, prepare yourselves!"
 gets
 cls
-puts "#{username1}\'s move!"
+puts "#{username1}\'s move! Enter a grid id."
 gets
 cls
-puts "#{username2}\'s move!"
+puts "#{username2}\'s move! Enter a grid id."
 
