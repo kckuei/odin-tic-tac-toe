3d6a7466ddaa0933807195b802767c2d06cfa499
kckuei <kevinkuei@gmail.com>
2022-09-23 23:50:46 -0700

    Add tests for checking winner

--- spec/tictactoe_spec.rb
+++ spec/tictactoe_spec.rb
@@ -8,6 +8,12 @@ require_relative '../lib/game'
 
 # Tests module - testing functionality of the board functions
 
+# Should
+# check board is updated/changes value
+# check correct winner prediction
+# check puts to screens
+# check user input valid, etc.
+
 describe Game do
   describe '#define_grid_size' do
     subject(:game) { described_class.new }
@@ -35,18 +41,112 @@ describe Game do
   end
 
   describe '#check_winner' do
-    context 'when we have a winner' do
-      xit 'returns true' do
+    subject(:game) { described_class.new }
+
+    context 'when have winner' do
+      it 'returns true for horizontal' do
+        # Arrange
+        grid_size = 9
+        board = game.initialize_board(grid_size)
+        mapping = game.make_mapping(grid_size)
+        (1..grid_size).to_a.each do |i|
+          coords = mapping[i]
+          game.update_board(coords, 'x', board)
+        end
+        # Action
+        result = game.check_winner('x', board)
+        # Assert
+        expect(result).to be true
+      end
+
+      it 'returns true vertical' do
+        # Arrange
+        grid_size = 9
+        board = game.initialize_board(grid_size)
+        mapping = game.make_mapping(grid_size)
+        (1..73).step(grid_size).to_a.each do |i|
+          coords = mapping[i]
+          game.update_board(coords, 'x', board)
+        end
+        # Action
+        result = game.check_winner('x', board)
+        # Assert
+        expect(result).to be true
+      end
+
+      it 'returns true diaganol 1' do
+        # Arrange
+        grid_size = 9
+        board = game.initialize_board(grid_size)
+        mapping = game.make_mapping(grid_size)
+        (1..81).step(grid_size + 1).to_a.each do |i|
+          coords = mapping[i]
+          game.update_board(coords, 'x', board)
+        end
+        # Action
+        result = game.check_winner('x', board)
+        # Assert
+        expect(result).to be true
+      end
+
+      it 'returns true diaganol 2' do
+        # Arrange
+        grid_size = 9
+        board = game.initialize_board(grid_size)
+        mapping = game.make_mapping(grid_size)
+        (9..73).step(grid_size - 1).to_a.each do |i|
+          coords = mapping[i]
+          game.update_board(coords, 'x', board)
+        end
+        # Action
+        result = game.check_winner('x', board)
+        # Assert
+        expect(result).to be true
       end
     end
-    context 'when we do not have a winner' do
-      xit 'returns false' do
+
+    context 'when no winner' do
+      it 'returns false' do
+        # Arrange
+        grid_size = 9
+        board = game.initialize_board(grid_size)
+        # Action
+        result = game.check_winner('x', board)
+        # Assert
+        expect(result).to be false
+      end
+    end
+
+    context 'when checking x and o is the winner' do
+      it 'returns false for x' do
+        # Arrange
+        grid_size = 9
+        board = game.initialize_board(grid_size)
+        mapping = game.make_mapping(grid_size)
+        (1..grid_size).to_a.each do |i|
+          coords = mapping[i]
+          game.update_board(coords, 'o', board)
+        end
+        # Action
+        result = game.check_winner('x', board)
+        # Assert
+        expect(result).to be false
+      end
+
+      it 'returns true for o' do
+        # Arrange
+        grid_size = 9
+        board = game.initialize_board(grid_size)
+        mapping = game.make_mapping(grid_size)
+        (1..grid_size).to_a.each do |i|
+          coords = mapping[i]
+          game.update_board(coords, 'o', board)
+        end
+        # Action
+        result = game.check_winner('o', board)
+        # Assert
+        expect(result).to be true
       end
     end
   end
 end
-
-# check board is updated/changes value
-# check correct winner prediction
-# check puts to screens
-# check user input valid, etc.
